db52d170cc2e76319ee1b3495263766aec99978f
Freshwind01 <90686749+Freshwind01@users.noreply.github.com>
2026-03-02 18:37:39 +0300

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,19 @@
+'use strict';
+
+
 function factorial(n) {
   // ваш код...
+if (!(Number.isInteger(n)  &&  n >= 0))  {      //проверка на число и положительность
+  console.log('Необходимо положительное целое число');
+  return; }
+if ((n===0)|| (n===1)) {           //Если 0 или 1, сразу ответ = 1
+  return 1; }
+  
+let res=n;
+while (n>1)  {
+   
+  res=res*(n-1);
+  n--;
+ }
+return res;
 }
