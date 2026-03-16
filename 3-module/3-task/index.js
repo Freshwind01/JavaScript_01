79755f8d9785a5d07c1b9ca36b1c1890d98be023
Freshwind01 <90686749+Freshwind01@users.noreply.github.com>
2026-03-16 09:55:13 +0300

    Update index.js

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,13 +1,14 @@
-function camelize(str) {
-let arr = str.split('');
-let NewArr=[];
-for (let i=0; i<arr.length;i++)
- { 
-  if (arr[i]==='-') {arr[i+1]=arr[i+1].toUpperCase(); //Если попадается '-', то следующая за ним буква должна быть заглавной
-                     delete arr[i]; //удаляю '-', остается пустое место. Если другой метод использовать, то будет пересчитан массив, а этого мне не надо. 
-                    }      
- }
-NewArr = arr.filter(item => {return item !== '';});  //очистить массив от пустых элементов
-return NewArr.join(''); // собрать в строку
-}
-//элегантнее не получилось что-то у меня...
+let camelize = (str) => {
+  let hasLeadingDash = str[0] === '-';
+  return str
+    .split('-')
+    .filter(item => item !== '')
+    .map((word, index) => {
+      if (index === 0 && !hasLeadingDash) {
+        return word;
+      } else {
+        return word[0].toUpperCase() + word.slice(1);
+      }
+    })
+    .join('');
+};
