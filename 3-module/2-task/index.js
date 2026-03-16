b7f6d9c30e66d37f14d4fd12a2c738822f10bd37
Freshwind01 <90686749+Freshwind01@users.noreply.github.com>
2026-03-16 09:58:47 +0300

    Update index.js

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,7 +1,3 @@
 function filterRange(arr, a, b) {
-  let newArr=[];
-  for (let el of arr)  {
-    if ((el>=a)&&(el<=b)) {newArr.push(el);}
-  }
-  return newArr;  
+  return arr.filter(el => el >= a && el <= b);
 }
