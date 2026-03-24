38eeab4617fe306f6dcda3b414ff02c1b7384140
Freshwind01 <90686749+Freshwind01@users.noreply.github.com>
2026-03-25 02:57:47 +0300

    Update index.js

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,5 @@
 function toggleText() {
-  // ваш код...
-}
+ let button = document.querySelector('.toggle-text-button');
+ let hide = document.getElementById('text');
+ button.addEventListener('click', function () { hide.hidden = !hide.hidden; });
+ }
