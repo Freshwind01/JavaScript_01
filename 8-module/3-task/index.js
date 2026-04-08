7da706e5cc5d1ad9220b5fbde26de997167b4f84
Freshwind01 <90686749+Freshwind01@users.noreply.github.com>
2026-04-08 19:55:51 +0300

    Update index.js

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,50 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) {
+      return;
+    }
+
+    let cartItem = this.cartItems.find(item => item.product.id === product.id);
+
+    if (!cartItem) {
+      cartItem = {
+        product: product,
+        count: 1
+      };
+      this.cartItems.push(cartItem);
+    } else {
+      cartItem.count++;
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let cartItemIndex = this.cartItems.findIndex(item => item.product.id === productId);
+    let cartItem = this.cartItems[cartItemIndex];
+
+    if (cartItem) {
+      cartItem.count += amount;
+
+      if (cartItem.count === 0) {
+        this.cartItems.splice(cartItemIndex, 1);
+      }
+
+      this.onProductUpdate(cartItem);
+    }
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => total + item.count, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => total + item.product.price * item.count, 0);
   }
 
   onProductUpdate(cartItem) {
